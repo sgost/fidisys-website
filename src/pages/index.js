9c369194bc1a1b7bfd1b7586eb8fc5752a361cef
Mohanapriya <priyamecheri@gmail.com>
2021-02-09 12:28:58 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => {
+  return (
+    <Fragment>
+      <SEO title="fidisys - Home" description="We design and develop beautiful applications, experience and brands that breaks the barriers of time and burn into the memories of customers love." keywords={['Mobile Applications development', 'Web Applications Development', 'Chennai Startup', 'UI/UX Designs', 'API Development', 'Devops', 'React', 'ReactNative', 'Angular', 'Vue', 'Kotlin', 'Flutter', 'Ionic', 'Android', 'IOS', 'Invision', 'Figma', 'Rails', 'NodeJS', 'Design System', 'Micro Front Ends']} />
+      <div>Home</div>
+    </Fragment>
+  )
+}
+
+export default IndexPage
